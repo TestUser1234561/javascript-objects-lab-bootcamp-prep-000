eb864541cc6410becec0cb7eb872dd68b25962d3
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 02:45:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+let recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({ key : value }, object)
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
